475b568d87e0d5e5d591d8da9448dae21d3eb003
Joseph Park <parkjoseph54@gmail.com>
2022-12-30 18:58:30 +0000

    in progress

--- client/components/hello-world.jsx
+++ client/components/hello-world.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 
-export default function HelloWorld(props) {
-  return (
-    <h1>Hello, World!</h1>
-  );
+export default class HelloWorld extends React.Component {
+  render() {
+    return (
+      <h1>Hello, World!</h1>
+    );
+  }
 }
