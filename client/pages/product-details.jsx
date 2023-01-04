373af61e18347ec96cb6f980f406cb9ae9786675
Joseph Park <parkjoseph54@gmail.com>
2023-01-04 19:44:55 +0000

    completed feature #2 - view product details

--- client/pages/product-details.jsx
+++ client/pages/product-details.jsx
@@ -5,10 +5,9 @@ import Image from '../../server/public/product-detail-img.jpeg';
 import Container from '@mui/material/Container';
 import Item from '@mui/material/ListItem';
 import Grid from '@mui/material/Grid';
-import ToggleButton from '@mui/material/ToggleButton';
-import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
 import Button from '@mui/material/Button';
 import { createTheme } from '@mui/material/styles';
+import Stack from '@mui/material/Stack';
 
 const theme = createTheme({
   palette: {
@@ -32,6 +31,7 @@ const styles = {
   },
   spacing: {
     marginTop: 0,
+    height: '22px',
     fontFamily: 'eczar',
     fontWeight: 300
   }
@@ -67,11 +67,11 @@ export default class ProductDetails extends React.Component {
     const sizeInputs = sizes.map(size => {
 
       return (
-        <Item key={size} style={{ padding: '0.2rem' }}>
-          <ToggleButton sx={{ width: '55px' }} >
-            {size}
-          </ToggleButton>
-        </Item>
+        <label key={size}>
+          <input className="size-input" type="radio" value={size} id={size}
+          name="sizes" onChange={this.handleChange} />
+          <span className='size'>{size}</span>
+        </label>
       );
     });
     return sizeInputs;
@@ -86,31 +86,43 @@ export default class ProductDetails extends React.Component {
         <Paper style={styles.paperContainer}>
           <NavBar />
         </Paper>
-        <Container maxWidth='md' >
-          <Grid container>
-            <Grid item xs={6} >
-              <Item><img style={{ width: 400, height: 400 }}
+
+        <Container maxWidth='md' style={{ marginTop: '1rem' }}>
+          <Grid container columns={{ xs: 4, sm: 8, md: 11 }}>
+            <Grid item xs={5} >
+              <Item style={{ padding: 0, justifyContent: 'center' }}><img style={{ width: 388, height: 390 }}
                   src={product.imageUrl}
                   srcSet={product.imageUrl}
                   alt={product.title}
                   loading="lazy"
                 /></Item>
             </Grid>
-            <Grid item xs={5} style={{ marginTop: '5rem' }}>
+
+            <Grid item xs={5} style={{ marginTop: 0, marginLeft: '1rem' }}>
               <span>
                 <h3 style={{
                   fontFamily: 'eczar',
                   fontWeight: 300,
                   marginBottom: 0
+
                 }}>{product.name}</h3>
                 <h3 style={styles.spacing}>${product.price}</h3>
                 <h3 style={styles.spacing}>size</h3>
               </span>
-
-              <ToggleButtonGroup exclusive onChange={this.handleChange} singleClick>
+              <Stack direction='row'>
                 {this.sizes()}
-              </ToggleButtonGroup>
-              <Button theme={theme} color='primary' variant='contained' style={{ width: '330px', marginTop: '3rem' }} href=''>ADD TO CART</Button>
+              </Stack>
+              <Button theme={theme} color='primary' variant='contained'
+              style={{ width: '330px', marginTop: '1.5rem' }} href=''>
+                ADD TO CART
+              </Button>
+              <div>
+                <ul>
+                  <li><p>SKU: {product.sku}</p></li>
+                  <li><p>100% Authencity Guaranteed</p></li>
+                  <li><p>In stock & ready to ship!</p></li>
+                </ul>
+              </div>
             </Grid>
           </Grid>
         </Container>
