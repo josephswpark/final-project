8d26c556873dd3db76f099968d54d190ad8a7aa8
Joseph Park <parkjoseph54@gmail.com>
2023-01-05 23:52:29 +0000

    fixed post method with the token issue

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const staticMiddleware = require('./static-middleware');
 const errorMiddleware = require('./error-middleware');
 const jsonMiddleware = express.json();
 const ClientError = require('./client-error');
-const authMiddleware = require('./auth-middleware');
+// const authMiddleware = require('./auth-middleware');
 const jwt = require('jsonwebtoken');
 const pg = require('pg');
 const app = express();
@@ -77,6 +77,8 @@ app.post('/api/addToCart', (req, res, next) => {
       .then(result => {
         const cartId = result.rows[0];
         const payload = cartId;
+        const token = jwt.sign(payload, process.env.TOKEN_SECRET);
+
         const cartItem = req.body;
         const { productId, quantity, size } = cartItem;
         const sql = `
@@ -114,35 +116,82 @@ app.post('/api/addToCart', (req, res, next) => {
   }
 });
 
-app.get('/api/cart', (req, res, next) => {
-  const token = req.get('X-Access-Token');
-  if (!token) {
-    return res.json([]);
-  }
-  const payload = jwt.verify(token, process.env.TOKEN_SECRET);
-  const cartId = payload.cartId;
-  const sql = `
-  select "productId",
-         "name",
-         "price",
-         "size",
-         "quantity",
-         "imageUrl"
-    from "cart"
-    join "cartItems" using("cartId")
-    join "shoes" using("productId")
-   where "cartId" = $1
-`;
-  const params = [cartId];
-  db.query(sql, params)
-    .then(result => {
-      const cartItems = result.rows;
-      res.status(200).json(cartItems);
-    })
-    .catch(err => next(err));
-});
+// app.get('/api/cart', (req, res, next) => {
+//   const token = req.get('X-Access-Token');
+//   if (!token) {
+//     return res.json([]);
+//   }
+//   const payload = jwt.verify(token, process.env.TOKEN_SECRET);
+//   const cartId = payload.cartId;
+//   const sql = `
+//   select "productId",
+//          "name",
+//          "price",
+//          "size",
+//          "quantity",
+//          "imageUrl"
+//     from "cart"
+//     join "cartItems" using("cartId")
+//     join "shoes" using("productId")
+//    where "cartId" = $1
+// `;
+//   const params = [cartId];
+//   db.query(sql, params)
+//     .then(result => {
+//       const cartItems = result.rows;
+//       res.status(200).json(cartItems);
+//     })
+//     .catch(err => next(err));
+// });
 
-app.use(authMiddleware);
+// app.post('/api/shoes', (req, res, next) => {
+//   const token = req.get('X-Access-Token');
+//   if (!token) {
+//     const cartSql = `
+//       insert into "cart" ("purchased")
+//               values ('false')
+//       returning "cartId"
+//     `;
+//     db.query(cartSql)
+//       .then(result => {
+//         const cartId = result.rows[0];
+//         const payload = cartId;
+//         const token = jwt.sign(payload, process.env.TOKEN_SECRET);
+//         const cartItem = req.body;
+//         const { productId, quantity, size } = cartItem;
+//         const sql = `
+//           insert into "cartItems" ("cartId", "productId", "quantity", "size")
+//           values ($1, $2, $3, $4)
+//           returning *
+//         `;
+//         const params = [payload.cartId, productId, quantity, size];
+//         db.query(sql, params)
+//           .then(result => {
+//             res.json({ token, cartItem: result.rows[0] });
+//           })
+//           .catch(err => next(err));
+//       })
+//       .catch(err => next(err));
+//   } else {
+//     const payload = jwt.verify(token, process.env.TOKEN_SECRET);
+//     const cartId = payload.cartId;
+//     const cartItem = req.body;
+//     const { productId, quantity, size } = cartItem;
+//     const sql = `
+//       insert into "cartItems" ("cartId", "productId", "quantity", "size")
+//       values ($1, $2, $3, $4)
+//       returning *
+//     `;
+//     const params = [cartId, productId, quantity, size];
+//     db.query(sql, params)
+//       .then(result => {
+//         res.json({ token, cartItem: result.rows[0] });
+//       })
+//       .catch(err => next(err));
+//   }
+// });
+
+// app.use(authMiddleware);
 app.use(errorMiddleware);
 
 app.listen(process.env.PORT, () => {
